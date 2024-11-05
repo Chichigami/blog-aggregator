079c9020dfbb8054de42ba89572a20c572ff579a
Chichigami <g.feng.work@gmail.com>
2024-11-05 16:35:27 -0500

    added feeds db schema, query to make feed, query to get all feeds along with user, added functionality to fetch the feed

--- fetchfeed.go
+++ fetchfeed.go
@@ -3,11 +3,16 @@ package main
 import (
 	"context"
 	"encoding/xml"
+	"fmt"
 	"html"
 	"io"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/chichigami/blog-aggregator/internal/database"
 	"github.com/chichigami/blog-aggregator/internal/rss"
+	"github.com/google/uuid"
 )
 
 func fetchFeed(ctx context.Context, feedURL string) (*rss.RSSFeed, error) {
@@ -36,4 +41,41 @@ func fetchFeed(ctx context.Context, feedURL string) (*rss.RSSFeed, error) {
 		feed.Channel.Item[i].Description = html.UnescapeString(feed.Channel.Item[i].Description)
 	}
 	return &feed, nil
+}
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.args) < 2 {
+		fmt.Println("need a title and url")
+		os.Exit(1)
+	}
+	name := cmd.args[0]
+	url := cmd.args[1]
+	user_id, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return err
+	}
+	newFeed := database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+		Url:       url,
+		UserID:    user_id.ID,
+	}
+	_, fetchErr := s.db.CreateFeed(context.Background(), newFeed)
+	if fetchErr != nil {
+		return err
+	}
+	return nil
+}
+
+func handlerFeeds(s *state, cmd command) error {
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, v := range feeds {
+		fmt.Printf("Name of Feed: %s\nURL: %s\nSubmitted by: %s\n", v.FeedName, v.Url, v.UserName)
+	}
+	return nil
 }
